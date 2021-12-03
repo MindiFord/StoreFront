48e271492395fc37dc7c9b398891ef9a3aa31166
Mindi Ford <MindiFord@live.com>
2021-12-02 22:35:35 -0600

    Added image utility & part of session-based shopping cart

---
+++ StoreFront.UI.MVC/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.UI.MVC.Models;
+using StoreFront.DATA;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            // Create local version of cart from session (global)
+            var shoppingCart = (Dictionary<int, CartItemViewModel>)Session["cart"];
+
+            if (shoppingCart == null || shoppingCart.Count == 0)
+            {
+                shoppingCart = new Dictionary<int, CartItemViewModel>();
+                ViewBag.Message = "There are no item in your cart.";
+            }
+            else
+            {
+                ViewBag.Message = null;
+            }
+
+            return View(shoppingCart);
+        }
+    }
+}
